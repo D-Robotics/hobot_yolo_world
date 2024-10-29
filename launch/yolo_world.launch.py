3e3f1ed3c3242d257ed0ac87572472a454368cfc
zixi01.chen <zixi01.chen@horizon.ai>
2024-10-29 08:30:42 +0000

    fix(yolo_world): fix launch

--- launch/yolo_world.launch.py
+++ launch/yolo_world.launch.py
@@ -40,7 +40,7 @@ def generate_launch_description():
         "yolo_world_dump_render_img", default_value=TextSubstitution(text="0")
     )
     dump_raw_launch_arg = DeclareLaunchArgument(
-        "yolo_world_dump_render_img", default_value=TextSubstitution(text="0")
+        "yolo_world_dump_raw_img", default_value=TextSubstitution(text="0")
     )
     model_file_name_launch_arg = DeclareLaunchArgument(
         "yolo_world_model_file_name", default_value=TextSubstitution(text="config/DOSOD_L_without_nms_nv12_v3.bin")
